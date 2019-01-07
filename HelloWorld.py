7f798f925d32e558a70cf04fbb975b210297559d
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-01-07 15:53:03 -0800

    Added a comment

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,5 @@
 Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> print("Hello World!")
+>>> print("Hello World!")       #Use the print command to print the output
 Hello World!
 >>> 
