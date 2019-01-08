21499925cfc3d2cfe3b866ba0522eea81cc83781
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-01-07 16:21:48 -0800

    Added Rules_for_variable_definition.py

---
+++ Rules_for_variable_definition.py
@@ -0,0 +1,17 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> totalMarks = 95
+>>> _totalMarks123 = 200
+>>> 123totalMarks = 100     # Doesnt start with digits first but with an alphabet or _ 
+SyntaxError: invalid syntax
+>>> totalMarks = 95
+>>> TotalMarks = 100
+>>> print(totalMarks)
+95
+>>> print(TotalMarks)
+100
+>>> for = 25                        # Cannot use reserved words as variables
+SyntaxError: invalid syntax
+>>> totalMarks$ = 100           # Cannot use special characters like $,#,etc
+SyntaxError: invalid syntax
+>>> 
