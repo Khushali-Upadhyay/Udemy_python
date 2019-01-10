f90b65946f5252e87323a7971e62f936acb6c1df
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-01-10 11:07:58 -0800

    Added Excercise_variables.py

---
+++ Exercise_variables.py
@@ -0,0 +1,13 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # Easy
+>>> marksObtained = 45
+>>> print(marksObtained)
+45
+>>> # Medium
+>>> break = 50            # A variable cannot be reserved word
+SyntaxError: invalid syntax
+>>> # Hard
+>>> phoneNumber = 12345
+>>> name123 = "Benny"
+>>> 
