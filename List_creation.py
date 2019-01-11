84366407a591a9cf4268c754baa3e2751f4447d9
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-01-11 11:03:52 -0800

    Added List_creation.py

---
+++ List_creation.py
@@ -0,0 +1,15 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # listName = [object1, object2, object3]
+>>> favouriteFruits = ["Apple", "Mango", "Strawberry"]
+>>> favouriteFruits
+['Apple', 'Mango', 'Strawberry']
+>>> favouriteFruits[0]
+'Apple'
+>>> favouriteFruits[2]
+'Strawberry'
+>>> # Modifying a list
+>>> favouriteFruits[1] = "Orange"
+>>> favouriteFruits
+['Apple', 'Orange', 'Strawberry']
+>>> 
