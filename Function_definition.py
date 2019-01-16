77611ec109ea6c9d35a9a6087af33acb2e83e932
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-01-15 20:52:59 -0800

    Function_definition.py

---
+++ Function_definition.py
@@ -0,0 +1,11 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #def functionName():
+>>> # blockOfCode
+>>> def helloWorld():
+	print("Hello, World!")
+
+	
+>>> helloWorld()
+Hello, World!
+>>> 
