d6ccea9115b89e0846e52f3a0d5b88ab8bc0c054
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-01-15 19:49:48 -0800

    Logical_operators_in_conditional_statements.py

---
+++ Logical_operators_in_conditional_statements.py
@@ -0,0 +1,34 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # and
+>>> 2<5 and 10> 7
+True
+>>> totalMarks = 95
+>>> percentageOfAttendence = 93
+>>> if totalMarks>=90 and percentageOfAttendence >= 90
+SyntaxError: invalid syntax
+>>> if totalMarks>=90 and percentageOfAttendence >= 90:
+	print("You are a disciplined student!")
+
+	
+You are a disciplined student!
+>>> percentageOfAttendence = 60
+>>> if totalMarks >=90 and percentageOfAttendence >=90:
+	print("You are a disciplined student!")
+
+	
+>>> # or
+>>> 2 > 5 or 10 > 7
+True
+>>> fruit = "Apple"
+>>> if fruit is "Mango" or fruit is "Apple":
+	print("I love that fruit!")
+
+	
+I love that fruit!
+>>> fruit = "Strawberry"
+>>> if fruit is "Mango" or fruit is "Apple":
+	print("I love that fruit!")
+
+	
+>>> 
