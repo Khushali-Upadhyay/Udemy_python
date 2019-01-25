3720fe91784da2e4d336f1bfb7eea1a31f25a619
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-01-24 22:15:16 -0800

    Boolean_and_comparison_operators_c2.py

---
+++ Boolean_and_comparison_operators_c2.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+True
+
+True
+
+# In[2]:
+
+
+False
+
+False
+
+
+# In[3]:
+
+
+True == 100
+
+False
+
+# In[4]:
+
+
+True == 1
+
+True
+
+# In[5]:
+
+
+False == -12
+
+False
+
+
+# In[6]:
+
+
+False == 0
+
+True
+
+
+# In[7]:
+
+
+not True
+
+False
+
+
+# In[9]:
+
+
+not False
+
+True
+
+
+# In[10]:
+
+
+True and True
+
+True
+
+
+# In[11]:
+
+
+True and False
+
+False
+
+
+# In[12]:
+
+
+False and False
+
+False
+
+
+# In[13]:
+
+
+True or False
+
+True
+
+
+# In[14]:
+
+
+10 == 10
+
+True
+
+
+# In[16]:
+
+
+10 != 10
+
+False
+
+
+# In[17]:
+
+
+10 != 2
+
+True
+
+
+# In[18]:
+
+
+200 >= 2001
+
+False
+
+
+# In[19]:
+
+
+500 <= 3000
+
+True
+
+
+# In[20]:
+
+
+10 > 1 or 4 != 4
+
+True
+
+
+# In[21]:
+
+
+10 > 1 and 4 != 4
+
+False
+
+
+# In[ ]:
+
+
+var1 = "house" ; var2 = "cinema" ; var3 = "temple"
+
+
+# In[27]:
+
+
+"c" not in var1
+
+True
+
+
+# In[33]:
+
+
+# Chain comparison operator
+24 >= 20 > 4 == 4 or 50 < 100 > 40 != 40
+
+True
+
+
+
+
+
