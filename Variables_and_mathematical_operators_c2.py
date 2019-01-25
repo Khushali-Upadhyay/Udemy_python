0705bd18cf95d53447d8c5985b646e6a8e3a54fe
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-01-24 22:46:47 -0800

    Variables_and_mathematical_operators_c2.py

---
+++ Variables_and_mathematical_operators_c2.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+var = 5
+print(40/var)
+print(40//var)
+print(40%var)
+print(40 - (8*var))
+
+8.0
+8
+0
+0
+
+
+# In[10]:
+
+
+var = 3
+print(40/var)
+print(40//var)
+print(40%var)
+print(40 - (13*var))
+
+13.333333333333334
+13
+1
+1
+
+
+# In[15]:
+
+
+x, y, z = 25, 80, 9.12345
+z
+
+9.12345
+
+
+# In[16]:
+
+
+x, y, z = 25, 80, 9.12345
+type(x)
+
+int
+
+
+# In[17]:
+
+
+x, y, z = 25, 80, 9.12345
+x_float = float(x)
+(x_float)
+
+25.0
+
+
+# In[19]:
+
+
+print(4*4*4)
+print(4**3)
+
+64
+64
+
+# In[ ]:
+
+
+
+
