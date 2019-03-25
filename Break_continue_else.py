ba9c450c92aedde1fc29e93a5eae194ef1125145
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-03-24 19:56:59 -0700

    Break_continue_else_c1.py

--- Break_continue_else.py
+++ Break_continue_else.py
@@ -1,5 +1,3 @@
-Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
 >>> # break     It stops loop from further execution
 >>> for number in range(1,11):
 	if number == 5:
