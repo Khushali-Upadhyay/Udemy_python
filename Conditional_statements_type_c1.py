a0a56726b00b3358293a99bce1d29beb7ed053f8
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-03-26 15:24:49 -0700

    Conditional_statements_type_c1.py

---
+++ Conditional_statements_type_c1.py
@@ -0,0 +1,43 @@
+>>> # If-condition
+>>> totalMarks = 95
+>>> if totalMarks >= 90:
+	print("Conratulations! You have secured an 'A' grade")
+
+	
+Conratulations! You have secured an 'A' grade
+>>> totalMarks = 80
+>>> if totalMarks >= 90:
+	print("Conratulations! You have secured an 'A' grade")
+
+	
+>>> # If- else condition
+>>> if totalMarks >= 90:
+	print("Congratulations! You have secured an 'A' grade")
+else:
+	print("You have cleared the examination")
+
+	
+You have cleared the examination
+>>> # If-elif condition
+>>> totalMarks = 60
+>>> if totalMarks >= 90:
+	print("Congratulations! You have secured an 'A' grade")
+elif totalMarks >= 40:
+	print("You have cleared the examination")
+
+else:
+	print("You have failed the examination")
+
+	
+You have cleared the examination
+>>> totalMarks = 100
+# Nested if
+>>> if totalMarks >= 90:
+	print("Congratulations! You have secured an 'A' grade")
+	if totalMarks ==100:
+		print("You have also secured full marks")
+
+		
+Congratulations! You have secured an 'A' grade
+You have also secured full marks
+>>> 
