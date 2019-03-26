84a18378421109969f3d0737d8e78cb9a2906cd0
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-03-26 15:36:38 -0700

    Dictionary_creation_c1.py

---
+++ Dictionary_creation_c1.py
@@ -0,0 +1,11 @@
+>>> # dictionaryName = {key1:value1, key2:value2}
+>>> # Dictionary to map price of a camera to its brand
+>>> priceOfCameras = {"Sony":500, "Nikon":600, "Canon":700}
+>>> priceOfCameras
+{'Sony': 500, 'Nikon': 600, 'Canon': 700}
+>>> priceOfCameras["Sony"]
+500
+>>> priceOfCameras["Nikon"] = 800
+>>> priceOfCameras["Nikon"]
+800
+>>> 
