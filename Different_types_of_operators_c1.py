1e409baf3b64fe2f80ec1a2bad7b1b6104bb0993
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-03-28 22:04:30 -0700

    Different_types_of_operators_c1.py

---
+++ Different_types_of_operators_c1.py
@@ -0,0 +1,52 @@
+>>> totalMarks = 95             # Assignment operator
+>>> print(totalMarks)
+95
+>>> 2 + 2                       # Arithmetic operator
+4
+>>> 10 - 8
+2
+>>> 4 * 4
+16
+>>> 10 / 5
+2.0
+>>> 10 // 5                      #Remainder would be an integer
+2
+>>> 15 % 10
+5
+>>> 2 < 5                        # Relational operator
+True
+>>> 5 < 5
+False
+>>> 5 <=5
+True
+>>> 6 <= 5
+False
+>>> 5 > 2
+True
+>>> 2 > 5
+False
+>>> 5 >= 5
+True
+>>> 5 >= 6
+False
+>>> 5 == 5
+True
+>>> 2 == 5
+False
+>>> 5 != 5
+False
+>>> 2 != 5
+True
+>>> 2<5 and 5<10                 # Logical operator
+True
+>>> 2<5 and 5>10
+False
+>>> 2<5 or 5<10
+True
+>>> 5>10 or 2<5
+True
+>>> 2>5
+False
+>>> not(2>5)
+True
+>>> 
