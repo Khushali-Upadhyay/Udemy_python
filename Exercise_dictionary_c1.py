76fff19a87a1ddae85d36813ae92fe16dc036b08
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-04-02 15:30:42 -0700

    Excercise_dictionary_c1.py

---
+++ Exercise_dictionary_c1.py
@@ -0,0 +1,16 @@
+>>> # Easy
+>>> colourOfVegetables = {"Okra":"Green","Eggplant":"Purple","Potato":"Brown","Carrot":"Orange","Lemon":"Yellow"}
+>>> colourOfVegetables
+{'Okra': 'Green', 'Eggplant': 'Purple', 'Potato': 'Brown', 'Carrot': 'Orange', 'Lemon': 'Yellow'}
+>>> # Medium
+>>> colourOfVegetables["Chilli"]="Red"
+>>> colourOfVegetables
+{'Okra': 'Green', 'Eggplant': 'Purple', 'Potato': 'Brown', 'Carrot': 'Orange', 'Lemon': 'Yellow', 'Chilli': 'Red'}
+>>> # Hard
+>>> colourOfVegetablesCopy = colourOfVegetables.copy()
+>>> colourOfVegetablesCopy
+{'Okra': 'Green', 'Eggplant': 'Purple', 'Potato': 'Brown', 'Carrot': 'Orange', 'Lemon': 'Yellow', 'Chilli': 'Red'}
+>>> colourOfVegetables.clear()
+>>> colourOfVegetables
+{}
+>>> 
