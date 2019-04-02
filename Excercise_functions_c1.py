5123cbbf3ce11f5940152720d413b24537aa5160
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-04-01 23:10:14 -0700

    Exercise_functions_c1.py

---
+++ Excercise_functions_c1.py
@@ -0,0 +1,42 @@
+>>> # Easy
+>>> def helloWorld():
+	print("Hello World!")
+
+	
+>>> helloWorld()
+Hello World!
+>>> # Medium
+>>> number1 = 10
+>>> number2 = 20
+>>> def returnSum(number1, number2):
+	sum = number1 + number2
+	return sum
+
+>>> returnSum(number1, number2)
+30
+>>> # Hard
+>>> side1 = 5
+>>> side2 = 5
+>>> side3 = 5
+>>> def triangleType(side1, side2, side3):
+	if side1 == side2 == side3:
+		if side2 == side3:
+			return("Equilateral triangle")
+		else:
+			return("Isosceles traingle")
+	elif side2 == side3:
+		return("Isosceles traingle")
+	else:
+		return("Scalene triangle")
+	triangle = triangleType(side1, side2, side3)
+	print(traingle)
+
+	
+>>> traingleType(side1, side2, side3)
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    traingleType(side1, side2, side3)
+NameError: name 'traingleType' is not defined
+>>> triangleType(side1, side2, side3)
+'Equilateral triangle'
+>>> 
