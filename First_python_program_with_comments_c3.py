7b717554c50ac00339f17af0ba710a405884d898
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-05-20 14:26:35 -0700

    First_python_program_with_comments_c3.py

---
+++ First_python_program_with_comments_c3.py
@@ -0,0 +1,7 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Python is easy")
+Python is easy
+'''Author is Khushali Upadhyay created on 20 th May 2019'''
+#This is my first python program
+
