d21af14beb27425ccd828f17b6f0f56762e7a136
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-05-21 17:22:55 -0700

    Datatypes_c3.py

---
+++ Datatypes_c3.py
@@ -0,0 +1,67 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Datatypes in Python
+>>> #None type whic does not contain any value
+>>> #Numeric type which contain integer, float, complex
+>>> #Sequences which contain str, bytes, bytearray, list,tuple, range
+>>> #Sets
+>>> #Mapping which is used to map and reduce
+>>> #Sets which do not allow duplicates
+>>> #Numeric datatypes
+>>> a=13
+>>> b=100
+>>> c=-66
+>>> print(a,b,c)
+13 100 -66
+>>> print(type(a))
+<class 'int'>
+>>> x=33.5
+>>> y=-25.8
+>>> z=205.0
+>>> print(x,y,z)
+33.5 -25.8 205.0
+>>> print(type(a))
+<class 'int'>
+>>> d=3+5j
+>>> print(d)
+(3+5j)
+>>> print(type(d))
+<class 'complex'>
+>>> e=0B1010
+>>> print(e)
+10
+>>> print(type(e))
+<class 'int'>
+>>> f=0XFF
+>>> print(f)
+255
+>>> print(type(f))
+<class 'int'>
+>>> g=True
+>>> print(g)
+True
+>>> print(type(g))
+<class 'bool'>
+>>> print(9<8)
+False
+>>> print(9>8)
+True
+>>> h=int(x)
+>>> print(x)
+33.5
+>>> print(h)
+33
+>>> print(type(h))
+<class 'int'>
+>>> i=float("22.5")
+>>> print(i)
+22.5
+>>> print(type(i))
+<class 'float'>
+>>> print(hex(10))
+0xa
+>>> print(oct(10))
+0o12
+>>> print(bin(10))
+0b1010
+>>> 
