6f97cc42ff7c7d93d361a2d74020eae1ae67441d
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-05-25 09:20:27 -0700

    Datatypes_assignment_c3.py

---
+++ Datatypes_assignment_c3.py
@@ -0,0 +1,15 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=10
+>>> print(a)
+10
+>>> b=20.54
+>>> print(b)
+20.54
+>>> c=True
+>>> print(c)
+True
+>>> d="I am the best"
+>>> print(d)
+I am the best
+>>> 
