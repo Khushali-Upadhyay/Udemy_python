b8a815c22589538a61e75beacbe299133e02a72e
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-05-30 10:55:09 -0700

    While_loop_implementation_c1.py

---
+++ While_loop_implementation_c1.py
@@ -0,0 +1,138 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> temperature = 77
+>>> # 68 - 77 degree fahrenheit
+>>> while temperature >= 68 and temperature <=77:
+	print("Room temperature is mainatined at {} degree fahrenheit".format(temperature))
+	temperature = temperature - 1
+
+	
+Room temperature is mainatined at 77 degree fahrenheit
+Room temperature is mainatined at 76 degree fahrenheit
+Room temperature is mainatined at 75 degree fahrenheit
+Room temperature is mainatined at 74 degree fahrenheit
+Room temperature is mainatined at 73 degree fahrenheit
+Room temperature is mainatined at 72 degree fahrenheit
+Room temperature is mainatined at 71 degree fahrenheit
+Room temperature is mainatined at 70 degree fahrenheit
+Room temperature is mainatined at 69 degree fahrenheit
+Room temperature is mainatined at 68 degree fahrenheit
+>>> while True:
+	print("This loop runs forever")
+
+	
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs forever
+This loop runs foreverTraceback (most recent call last):
+  File "<pyshell#16>", line 2, in <module>
+    print("This loop runs forever")
+KeyboardInterrupt
+>>> 
