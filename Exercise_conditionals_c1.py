084848f5e23613fb2b1a1dfd204b1807c84bbfc2
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-05-30 11:09:47 -0700

    Exercise_loops_c1.py

---
+++ Exercise_conditionals_c1.py
@@ -0,0 +1,12 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # Easy
+>>> number = 8
+>>> if number <= 10:
+	print("The number is less than 10")
+
+	
+The number is less than 10
+>>> # Medium
+>>> number = 6
+>>> if number 
