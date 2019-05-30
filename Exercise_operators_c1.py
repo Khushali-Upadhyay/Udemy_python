f5dbf11f13bb88b1d8a960c0e6202c15f2f1b6d2
Khushali-Upadhyay <khushalimupadhyay@gmail.com>
2019-05-30 10:09:15 -0700

    Exercise_operators_c1.py

---
+++ Exercise_operators_c1.py
@@ -0,0 +1,12 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # Easy
+>>> 4 + 5
+9
+>>> # Medium
+>>> 2 + 3 * 4
+14
+>>> # Hard
+>>> 2 < 5 and 5 <8
+True
+>>> 
